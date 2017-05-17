162871497b6e5b56973b28ace3125536a2c74f7c
Robert Puls <rpu1s@sbcglobal.net>
2017-05-16 19:37:45 -0600

    add search functionality

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
-let $data = $.get("https://tacos.now.sh/", function(){
-  console.log($data.responseJSON);
-  let base = [];
-  let td1 =[];
-  let td =[];
-  let myStr = "Chicken";
-  for (var i = 0; i < $data.responseJSON.base_layers.length; i++) {
-    if ($data.responseJSON.base_layers[i].title.indexOf(myStr) != -1) {
-      console.log("found");
-      console.log($data.responseJSON.base_layers[i].title);
-    }
-  }
+    var $data = $.get("https://tacos.now.sh/", function() {
+      console.log($data.responseJSON);
 
-  // let i = 0;
-  // for (var key in $data.responseJSON) {
-  //   if ($data.responseJSON.hasOwnProperty(key)) {
-  //     console.log($data.responseJSON[key][0].title);
-  //     td1.push(document.createElement("td"));
-  //     tr.push(document.createElement("tr"));
-  //     td.push(document.createElement("td"));
-  //     td[i].innerText = $data.responseJSON[key].title;
-  //     td1[i].innerText = $data.responseJSON[key][0].title;
-  //     tr[i].append(td[i]);
-  //     tr[i].append(td1[i]);
-  //     $("tbody")[0].append(tr[i]);
-  //   }
-  //   i++;
-  // }
-});
+
+
+
+      $("select").on("change", function() {
+        let base = [];
+        let myStr = this.children;
+        for (var i = 0; i < myStr.length; i++) {
+          if (myStr[i].selected) {
+            myStr = myStr[i].innerText;
+            console.log(myStr);
+          }
+        }
+        let listId = this.value;
+        console.log(listId);
+
+
+
+        $("#" + listId)[0].innerHTML = "";
+
+        $("#" + listId).append("<ul id=" + listId + 1 + " class=\"collapsible\" data-collapsible=\"accordion\"></ul>");
+        for (let i = 0; i < $data.responseJSON[listId].length; i++) {
+          if ($data.responseJSON[listId][i].title.indexOf(myStr) != -1) {
+            $("#" + listId + "1").append("<li value=" + i + " id=\"test" + i + "\"><div class=\"collapsible-header\">" + $data.responseJSON[listId][i].title + "</li>");
+            $("#test" + i).append("<ul id=\"1test" + i + "\" class=\"collapsible-body\"></ul>");
+            for (let j = 0; j < $data.responseJSON[listId][i].ingredients.length; j++) {
+              base.push($data.responseJSON[listId][i].ingredients[j]);
+              $("#1test" + i).append("<li class=\"list\">" + base[j] + "</li>");
+            }
+            $(".collapsible").collapsible();
+          }
+        }
+      });
+    });
